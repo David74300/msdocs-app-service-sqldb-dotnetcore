9f59a6d40b4bc3be34faa4f5ebea39f7795214d6
David74300 <davidruiz396693@gmail.com>
2024-09-11 08:45:46 +0000

    rerere

--- Controllers/TodosController.cs
+++ Controllers/TodosController.cs
@@ -86,10 +86,10 @@ namespace DotNetCoreSqlDb.Controllers
         // The cache logic is added with the help of GitHub Copilot
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Description,CreatedDate,Nombre")] Todo todo)
+        public async Task<IActionResult> Create([Bind("ID,Description,CreatedDate")] Todo todo)
         {
             if (ModelState.IsValid)
             {
